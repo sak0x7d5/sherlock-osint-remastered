570dd9d39bd393140ab2d218bea697809319063e
sak0x7d5 <70936061+sak0x7d5@users.noreply.github.com>
2026-05-18 17:19:11 +0500

    test: update route redirect tests to use fetch_with_page and fetch_with_api

--- tests/test_playwright_engine.py
+++ tests/test_playwright_engine.py
@@ -20,6 +20,19 @@ def test_get_request_fn_returns_callable(valid_method: str, playwright_engine: P
         (302, r'https://httpbin.org/redirect-to?url=https%3A%2F%2Fhttpbin.org%2F&status_code=302'),
         (200, r'https://httpbin.org/status/200')
         ])
-async def test_handle_route_redirect_site(status: int, url: str, playwright_engine: PlaywrightEngine):
-    resp = await playwright_engine.fetch_site(url=url, max_redirects=0)
+async def test_handle_route_redirect_site_with_page(status: int, url: str, playwright_engine: PlaywrightEngine):
+    resp = await playwright_engine.fetch_with_page(url=url, max_redirects=0)
+    assert resp.status == status
+
+
+@pytest.mark.asyncio()
+@pytest.mark.online
+@pytest.mark.parametrize(
+    "status,url", [
+        (302, r'https://httpbin.org/redirect-to?url=https%3A%2F%2Fhttpbin.org%2F&status_code=302'),
+        (200, r'https://httpbin.org/status/200')
+        ])
+async def test_handle_route_redirect_site_with_api(status: int, url: str, playwright_engine: PlaywrightEngine):
+    request_fn = playwright_engine.context.request.head
+    resp = await playwright_engine.fetch_with_api(request_fn=request_fn, url=url, max_redirects=0)
     assert resp.status == status
